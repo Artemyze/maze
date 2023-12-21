f8eb467baef11378fb3f4c05d174f757c3d41d30
Artemyze <mironovarym@gmail.com>
2023-12-21 20:37:33 +0300

    Add docs

--- main.py
+++ main.py
@@ -8,10 +8,50 @@ from maze import map_generator
 from os import path
 from PIL import Image
 
+"""
+Программа визуализации лабиринта
+
+Этот скрипт определяет класс Program, который генерирует и визуализирует лабиринты на основе указанных параметров.
+Код генерации лабиринта - реализация Алгоритма Прима.  Он также относится к классу алгоритмов создания лабиринтов и 
+использует случайные элементы для генерации структуры лабиринта. Принцип работы алгоритма заключается в том,
+что начиная с одной случайной точки, он поочередно добавляет проходы в лабиринте, соединяя текущую ячейку с 
+соседней ячейкой, которая уже принадлежит лабиринту. 
+Код поиска пути реализует алгоритм генерации лабиринта на основе поиска в глубину (Depth-First Search, DFS). Этот 
+алгоритм отлично подходит для создания лабиринтов, так как он создает пути, случайным образом выбирая направления и 
+соединяя ячейки лабиринта.
+Он использует модуль argparse для анализа аргументов командной строки с целью настройки генерации лабиринта.
+
+Использование:
+    python script_name.py -image_output output_image.png -text_output output_text.txt
+                         -text_input input_text.txt -width 800 -height 600 -difficult medium
+
+Аргументы командной строки:
+    -image_output, --img_o   : Выходной файл изображения для сгенерированного лабиринта (по умолчанию: const.IMAGE_OUTPUT).
+    -text_output, --txt_o    : Выходной текстовый файл для сгенерированного лабиринта в формате .txt (по умолчанию: const.TEXT_OUTPUT).
+    -text_input, --txt_i     : Входной текстовый файл для генерации лабиринта в формате .txt (по умолчанию: const.TEXT_OUTPUT).
+    -width, --w              : Ширина изображения визуализации лабиринта (по умолчанию: const.WIDTH).
+    -height, --h             : Высота изображения визуализации лабиринта (по умолчанию: const.HEIGHT).
+    -difficult, --dif        : Параметр, определяющий сложность лабиринта (easy, medium, hard; по умолчанию определяются в словаре const.DIFFICULT_MAZE_BASE).
+
+"""
+
 
 class Program:
     def __init__(self, width, height, difficult_maze, img_out_fn, txt_out_fn, txt_in_fn):
+        """
+                Инициализация объекта Program.
+
+                Параметры:
+                    width (int): Ширина изображения лабиринта.
+                    height (int): Высота изображения лабиринта.
+                    difficult_maze (str): Уровень сложности лабиринта ('easy', 'medium', 'hard').
+                    img_out_fn (str): Имя файла для сохранения изображения лабиринта.
+                    txt_out_fn (str): Имя файла для сохранения текстового представления лабиринта.
+                    txt_in_fn (str): Имя входного файла для генерации лабиринта.
+        """
         self.dict_difficult_maze = {
+            # в этом словаре определяется количество путей в лабиринте для каждого
+            # уровня сложности
             'easy': (const.AMOUNT_CELLS_IN_H_EASY, const.AMOUNT_CELLS_IN_W_EASY),
             'medium': (const.AMOUNT_CELLS_IN_H_MEDIUM, const.AMOUNT_CELLS_IN_W_MEDIUM),
             'hard': (const.AMOUNT_CELLS_IN_H_HARD, const.AMOUNT_CELLS_IN_W_HARD)
@@ -24,20 +64,35 @@ class Program:
         self.txt_in_fn = txt_in_fn
         self.cells_sizes = self.dict_difficult_maze[f'{self.difficult_maze}']
         self._cell_size_h, self._cell_size_w = ((self.height / _ - 1) for _ in self.cells_sizes)
-        self.sl = None
-        self.map = None
-        self.solution = []
-        self.solution_data = []
+        self.sl = None  #здесь будут хранится список, хранящий наше решение для лабиринта
+        self.map = None  #массив для хранения информации об объекте в каждой координате
+        # (i = y, j = x если в координатной 2д плоскости)
+        self.solution = [] #полный список со всеми обходами ячеек, который совершил алгоритм решения лабиринта
         self.load_map(self.cells_sizes)
         self.canvas = Image.new('RGB', (self.width, self.height), (0, 0, 0))
         self.draw = Draw(self.canvas)
 
-    def load_map(self, difficult_maze):
+    def load_map(self, cells_sizes):
+        """
+                Загрузка карты лабиринта на основе уровня сложности.
+
+                Параметры:
+                    difficult_maze (tuple): кортеж с количеством ячеек по вертикали и по горизонтали.
+        """
         folder = path.dirname(__file__)
-        self.sl, self.solution = map_generator(*difficult_maze, self.txt_in_fn)
+        self.sl, self.solution = map_generator(*cells_sizes, self.txt_in_fn) #  формируем в списках решение
         self.map = Map(path.join(folder, self.txt_in_fn))
 
     def _recty(self, i, j, color, k=1):
+        """
+                Вспомогательный метод для отрисовки прямоугольника на изображении.
+
+                Параметры:
+                    i (int): Индекс строки.
+                    j (int): Индекс столбца.
+                    color (str): Цвет прямоугольника.
+                    k (float): Множитель размера прямоугольника (по умолчанию 1).
+        """
         y = self._cell_size_h * i
         x = self._cell_size_h * j
         self.draw.rectangle((x, y,
@@ -45,8 +100,30 @@ class Program:
                             fill=color)
 
     def rendering(self):
+        """
+               Метод визуализации лабиринта на изображении.
 
+               Метод включает в себя три подметода для отрисовки различных элементов лабиринта:
+               1. _render_initial_objects - отрисовка начального состояния лабиринта (стены, пути и т. д.).
+               2. _render_solution_with_all_steps - отрисовка пути решения лабиринта с учетом всех шагов.
+               3. _render_pure_solution - отрисовка чистого пути решения лабиринта без дополнительных элементов.
+
+               Параметры:
+                   Нет.
+
+               Возвращаемые значения:
+                   Нет.
+        """
         def _render_initial_objects():
+            """
+                Подметод для отрисовки начального состояния лабиринта.
+
+                Параметры:
+                    Нет.
+
+                Возвращаемые значения:
+                    Нет.
+            """
             dict_objects_in_maze = {
                 2: 'blue',
                 1: 'white',
@@ -58,6 +135,15 @@ class Program:
                     self._recty(ind_i, ind_j, dict_objects_in_maze[self.map.data[ind_i][ind_j]])
 
         def _render_solution_with_all_steps():
+            """
+                Подметод для отрисовки пути решения лабиринта с учетом всех шагов.
+
+                Параметры:
+                    Нет.
+
+                Возвращаемые значения:
+                    Нет.
+            """
             for col, k in self.solution:
                 if col == 1:
                     color = 'yellow'
@@ -69,6 +155,15 @@ class Program:
                     self._recty(i, j, color)
 
         def _render_pure_solution():
+            """
+                Подметод для отрисовки чистого пути решения лабиринта без дополнительных элементов.
+
+                Параметры:
+                        Нет.
+
+                Возвращаемые значения:
+                        Нет.
+            """
             for i in range(len(self.sl)):
                 for j in range(len(self.sl)):
                     if self.sl[i][j] == 1:
@@ -80,6 +175,12 @@ class Program:
         self.save_img(self.img_out_fn)
 
     def save_img(self, file_name):
+        """
+            Сохранение изображения лабиринта.
+
+            Параметры:
+                file_name (str): Имя файла для сохранения изображения.
+        """
         self.canvas.save(file_name)
 
 
@@ -101,7 +202,7 @@ parser.add_argument(
 parser.add_argument(
     '-text_input', '--txt_i',
     type=str,
-    default=f'{const.TEXT_OUTPUT}',
+    default=None,
     help='имя входного файла для создания лабиринта(.txt формат)',
 )
 
--- options.py
+++ options.py
@@ -1,11 +1,15 @@
-import datetime
+# Установка ширины и высоты изображения
+WIDTH = 1280
+HEIGHT = 1024
 
-WIDTH = 1280  # root.winfo_screenwidth()
-HEIGHT = 1024  # root.winfo_screenheight()
-
+# Имя файла для сохранения изображения и текстового представления лабиринта
 IMAGE_OUTPUT = f'output.png'
 TEXT_OUTPUT = f'map.txt'
+
+# Определение базовой сложности лабиринта
 DIFFICULT_MAZE_BASE = 'medium'
+
+# Определение размеров лабиринта для разных уровней сложности
 AMOUNT_CELLS_IN_W_EASY = 20
 AMOUNT_CELLS_IN_H_EASY = 20
 AMOUNT_CELLS_IN_W_MEDIUM = 40
@@ -13,8 +17,8 @@ AMOUNT_CELLS_IN_H_MEDIUM = 40
 AMOUNT_CELLS_IN_W_HARD = 60
 AMOUNT_CELLS_IN_H_HARD = 60
 
-COLOR_S = 2
-COLOR_WALL = 1
-COLOR_WAY = 0
-COLOR_FINISH = 3
-
+# Определение цветов для отображения элементов лабиринта
+COLOR_S = 2  # Цвет стартовой клетки
+COLOR_WALL = 1  # Цвет стен
+COLOR_WAY = 0  # Цвет свободного пространства
+COLOR_FINISH = 3  # Цвет финишной клетки
